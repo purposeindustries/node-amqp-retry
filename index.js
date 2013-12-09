01a5493b2e9077fa6df3ba056242c4b035667773
Dányi Bence <madbence@gmail.com>
2013-12-09 19:38:25 +0100

    Extract connection info from `job`

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
-exports.init = function init(conn) {
-  var schedule = require('amqp-schedule')(conn);
-  return function wrapper(maxTry, initialWait, cb) {
-    return function(message, header, deliveryInfo, job) {
-      header.retries = header.retries || 0;
-      job.retry = function() {
-        header.retries++;
-        if(header.retries >= maxTry) {
-          return cb(new Error('Message processing failed ' + maxTry + ' times'), message, header, deliveryInfo, job);
-        }
-        schedule(deliveryInfo.exchange, deliveryInfo.routingKey, message, Math.pow(2, header.retries)*initialWait, {
-          contentType: deliveryInfo.contentType,
-          headers: header
-        });
-      };
-      try {
-        cb(null, message, header, deliveryInfo, job);
-      } catch(e) { console.error(e); }
+module.exports = function wrapper(maxTry, initialWait, cb) {
+  return function(message, header, deliveryInfo, job) {
+    header.retries = header.retries || 0;
+    job.retry = function() {
+      header.retries++;
+      if(header.retries >= maxTry) {
+        return cb(new Error('Message processing failed ' + maxTry + ' times'), message, header, deliveryInfo, job);
+      }
+      require('amqp-schedule')(job.queue.connection)(deliveryInfo.exchange, deliveryInfo.routingKey, message, Math.pow(2, header.retries)*initialWait, {
+        contentType: deliveryInfo.contentType,
+        headers: header
+      });
     };
+    try {
+      cb(null, message, header, deliveryInfo, job);
+    } catch(e) { console.error(e); }
   };
 };
+
+var c = require('amqp').createConnection('amqp://localhost');
+c.once('ready', function() {
+  c.queue('test', function(q) {
+    q.subscribe(module.exports(5, 500, function(err, message, header, deliveryInfo, job) {
+      if(err) {
+        return console.log(err);
+      }
+      console.log(message);
+      job.retry();
+    }));
+    c.publish('test', {foo:'bar'}, { contentType: 'application/json' });
+  });
+});
