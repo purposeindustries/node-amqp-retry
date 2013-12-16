e41fcb0e7aefc0ac92db036234bb98108f5911ca
Bence Dányi <madbence@gmail.com>
2013-12-16 14:36:01 +0100

    Flexible wait times

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-module.exports = function wrapper(maxTry, initialWait, cb) {
+module.exports = function wrapper(initialWait, maxTry, cb) {
   return function(message, header, deliveryInfo, job) {
     header.retries = header.retries || 0;
-    job.retry = function() {
+    job.retry = function(wait, tries) {
+      if( wait === undefined ) {
+        wait = initialWait;
+      }
+      if(tries === undefined) {
+        tries = maxTry;
+      }
+      var delay = wait instanceof Array ? wait[header.retries] : Math.pow(2, header.retries)*wait;
       header.retries++;
-      if(header.retries >= maxTry) {
+      if(header.retries >= tries) {
         return cb(new Error('Message processing failed ' + maxTry + ' times'), message, header, deliveryInfo, job);
       }
-      require('amqp-schedule')(job.queue.connection)(deliveryInfo.exchange, deliveryInfo.routingKey, message, Math.pow(2, header.retries)*initialWait, {
+      require('amqp-schedule')(job.queue.connection)(deliveryInfo.exchange, deliveryInfo.routingKey, message, delay, {
         contentType: deliveryInfo.contentType,
         headers: header
       });
